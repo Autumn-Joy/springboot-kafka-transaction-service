5befcdd323d8dd1505ba00f75ca2aa058b2860a4
Autumn Hicks <ajhicks1511@gmail.com>
2026-02-25 14:29:50 -0500

    feat(incentive): refactor `TransactionKafkaListener` to call `TransactionService`

--- src/main/java/com/jpmc/midascore/component/TransactionKafkaListener.java
+++ src/main/java/com/jpmc/midascore/component/TransactionKafkaListener.java
@@ -5,6 +5,7 @@ import com.jpmc.midascore.entity.UserRecord;
 import com.jpmc.midascore.foundation.Transaction;
 import com.jpmc.midascore.repository.TransactionRecordRepository;
 import com.jpmc.midascore.repository.UserRepository;
+import com.jpmc.midascore.service.TransactionService;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -15,61 +16,20 @@ import org.springframework.stereotype.Component;
 @Component
 public class TransactionKafkaListener {
 
-    private final ValidateTransactionService transactionValidationService;
-    private final TransactionRecordRepository transactionRecordRepository;
-    private final UserRepository userRepository;
+    private final TransactionService transactionService;
 
-    public TransactionKafkaListener(ValidateTransactionService transactionValidationService, TransactionRecordRepository transactionRecordRepository, UserRepository userRepository) {
-        this.transactionValidationService = transactionValidationService;
-        this.transactionRecordRepository = transactionRecordRepository;
-        this.userRepository = userRepository;
+    public TransactionKafkaListener(TransactionService transactionService) {
+
+        this.transactionService = transactionService;
     }
 
     @KafkaListener(topics = "${general.kafka-topic}")
     public void receiveTransactions(Transaction transaction) {
 
-        processTransaction(transaction);
-        // TO DO: test 3 passes for this; refactor later
-        findWaldorfBalance();
+        transactionService.processTransaction(transaction);
+
+        // TO DO: test 3 passes for this; remove later
+        transactionService.returnTestValue();
 
     }
-
-    public String processTransaction(Transaction transaction) {
-        UserRecord sender = userRepository.findById(transaction.getSenderId());
-        UserRecord recipient = userRepository.findById(transaction.getRecipientId());
-
-        // TO DO: clean up debugging statements
-//        System.out.println("Sender info:" + sender);
-//        System.out.println("Recipient info: " + recipient);
-//        System.out.println("Transaction info: " + transaction);
-
-        //  validate transaction
-        if (!transactionValidationService.isValid(transaction)) {
-
-            // TO DO: clean up debugging statements
-//            System.out.println("Transaction is invalid");
-            return "Invalid transaction. Transaction discarded.";
-//            throw new Error("Invalid transaction. Transaction discarded. Please try again and ensure that (1) the Sender Id and Recipient Id are valid and (2) that the sender's balance is equal or above the transaction amount.");
-        }
-        System.out.println("Transaction is valid");
-        //save transaction to database
-        transactionRecordRepository.save(new TransactionRecord(transaction));
-        System.out.println("Transaction saved to database");
-
-        //update sender balance and SAVE TO DB!
-        sender.setBalance(sender.getBalance() - transaction.getAmount());
-        userRepository.save(sender);
-
-        //update recipient balance and SAVE TO DB!
-        recipient.setBalance(recipient.getBalance() + transaction.getAmount());
-        userRepository.save(recipient);
-
-        return "Transaction saved to database and updated sender and recipient balances.";
-    }
-
-    public void findWaldorfBalance() {
-        System.out.println(userRepository.findById(5));
-    }
-
-
 }
